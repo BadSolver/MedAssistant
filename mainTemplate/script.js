335fbb8c99836ec9b4f112aa5ab1b73592a1019a
Alex Kurov <95409553+BadSolver@users.noreply.github.com>
2023-06-14 01:29:43 +0300

    add accordeon for main page

--- mainTemplate/script.js
+++ mainTemplate/script.js
@@ -1,8 +1,13 @@
-const accordionHeaders = document.querySelectorAll(".accordion-header");
-const accordionContents = document.querySelectorAll(".accordion-content");
+const accordionHeaders = document.querySelectorAll(".accordeon-header");
+const accordionContents = document.querySelectorAll(".accordeon-content");
 
 accordionHeaders.forEach((accordionHeader, index) => {
   accordionHeader.addEventListener("click", function () {
     accordionContents[index].classList.toggle("active");
+    accordionHeader.classList.toggle("active");
+    const accordionIcon = accordionHeader.querySelector(".accordeon-minus img");
+    accordionContents[index].classList.contains("active")
+      ? (accordionIcon.src = "./images/accordeon-minus.svg")
+      : (accordionIcon.src = "./images/accordeon-plus.svg");
   });
 });
