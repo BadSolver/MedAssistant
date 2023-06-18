ea2d10e08ac02060731166b714fd46afbd1dc8a8
Alex Kurov <95409553+BadSolver@users.noreply.github.com>
2023-06-18 19:07:50 +0300

    refactor: refact code

--- indication/index.js
+++ indication/index.js
@@ -1,23 +1,3 @@
-// const openModalBtn = document.querySelector('.description-vet-v2');
-// const closeModalBtn = document.querySelector('.close_modal_window');
-// const modal = document.querySelector('.modal');
-
-// openModalBtn.addEventListener('click', () => {
-//   modal.style.display = 'block';
-// });
-
-// closeModalBtn.addEventListener('click', () => {
-//   modal.style.display = 'none';
-// });
-
-// modal.addEventListener('click', (event) => {
-//   if (event.target === modal) {
-//     modal.style.display = 'none';
-//   }
-// });
-
-
-
 const openModalBtns = document.querySelectorAll('.modal-open-btn');
 const closeModalBtn = document.querySelector('.close_modal_window');
 const modal = document.querySelector('.modal');
