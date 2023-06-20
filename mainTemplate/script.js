f80e1cd4ff7ce2b16d353ee38ce6b72069ca223e
Alex Kurov <95409553+BadSolver@users.noreply.github.com>
2023-06-20 12:19:34 +0300

    Update script.js

--- mainTemplate/script.js
+++ mainTemplate/script.js
@@ -32,50 +32,14 @@ accordionHeaders.forEach((accordionHeader, index) => {
   });
 });
 
-<<<<<<< Updated upstream
-
-let btns = document.querySelectorAll("*[data-modal-btn]");
-
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    let name = btns[i].getAttribute("data-modal-btn");
-    let modal = document.querySelector("[data-modal-window='" + name + "']");
-    modal.style.display = "block";
-    let close = modal.querySelector(".close_modal_window");
-    close.addEventListener("click", function () {
-      modal.style.display = "none";
-    });
-  });
-}
-
-window.onclick = function (event) {
-  if (event.target.hasAttribute("data-modal-window")) {
-    let modals = document.querySelectorAll("*[data-modal-window]");
-    for (let i = 0; i < modals.length; i++) {
-      modals[i].style.display = "none";
-    }
-  }
-};
-
-const openModalBtns = document.querySelectorAll(".modal-open-btn");
-=======
 // Modal windows
 const openModalAuthBtns = document.querySelectorAll(".modal-open-btn");
->>>>>>> Stashed changes
 const closeModalBtns = document.querySelectorAll(".close_modal_window");
 const modalContainers = document.querySelectorAll(".modal");
 
 openModalBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     const modalTarget = btn.getAttribute("data-modal-target");
-<<<<<<< Updated upstream
-    const modal = document.querySelector(`[data-modal-window="${modalTarget}"]`);
-    modal.style.display = "block";
-
-    if (modalTarget === "moduleSignUp") {
-      const loginModal = document.querySelector('[data-modal-window="moduleLogIn"]');
-      loginModal.style.display = "none";
-=======
     const modal = document.querySelector(
       `[data-modal-window="${modalTarget}"]`
     );
@@ -86,13 +50,10 @@ openModalBtns.forEach((btn) => {
         '[data-modal-window="moduleLogIn"]'
       );
       closeModal(loginModal);
->>>>>>> Stashed changes
     }
   });
 });
 
-<<<<<<< Updated upstream
-=======
 const loginLink = document.querySelector(
   '[data-modal-window="moduleSignUp"] .main__link a'
 );
@@ -124,7 +85,6 @@ forgotPasswordLink.addEventListener("click", () => {
   openModal(passwordModal);
 });
 
->>>>>>> Stashed changes
 closeModalBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     const modal = btn.closest(".modal");
@@ -140,30 +100,6 @@ modalContainers.forEach((modal) => {
   });
 });
 
-<<<<<<< Updated upstream
-const loginLink = document.querySelector('[data-modal-window="moduleSignUp"] .main__link a');
-loginLink.addEventListener("click", () => {
-  const signUpModal = document.querySelector('[data-modal-window="moduleSignUp"]');
-  signUpModal.style.display = "none";
-
-  const loginModal = document.querySelector('[data-modal-window="moduleLogIn"]');
-  loginModal.style.display = "block";
-});
-
-const forgotPasswordLink = document.querySelector('[data-modal-window="moduleLogIn"] .main__link a');
-forgotPasswordLink.addEventListener("click", () => {
-  const openModals = document.querySelectorAll('.modal');
-  openModals.forEach((modal) => {
-    modal.style.display = 'none';
-  });
-
-  const passwordModal = document.querySelector('[data-modal-window="modulePassword"]');
-  passwordModal.style.display = 'block';
-});
-
-
-
-=======
 const openModalQuestionBtn = document.querySelector(
   "[data-modal-btn='moduleQuestion']"
 );
@@ -176,4 +112,3 @@ openModalQuestionBtn.addEventListener("click", function () {
 });
 
 closeModalOnOutsideClick(modalWindow);
->>>>>>> Stashed changes
