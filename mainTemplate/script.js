e2538c130ff69629885a26b6295ad39713ef8bc1
Alex Kurov <95409553+BadSolver@users.noreply.github.com>
2023-06-20 20:42:01 +0300

    feat: return accoerdeon for mainPage

--- mainTemplate/script.js
+++ mainTemplate/script.js
@@ -8,6 +8,20 @@ function closeModal(modal) {
   modal.style.display = "none";
 }
 
+const accordionHeaders = document.querySelectorAll(".accordeon-header");
+const accordionContents = document.querySelectorAll(".accordeon-content");
+
+accordionHeaders.forEach((accordionHeader, index) => {
+  accordionHeader.addEventListener("click", function () {
+    accordionContents[index].classList.toggle("active");
+    accordionHeader.classList.toggle("active");
+    const accordionIcon = accordionHeader.querySelector(".accordeon-minus img");
+    accordionContents[index].classList.contains("active")
+      ? (accordionIcon.src = "../images/accordeon-minus.svg")
+      : (accordionIcon.src = "../images/accordeon-plus.svg");
+  });
+});
+
 // Функция закрытия модальных окон по клику вне модального окна
 function closeModalOnOutsideClick(modal) {
   window.addEventListener("click", function (event) {
